a3b64db89d35f6b4aaff1bd1910430dce83e1068
shyu <qaz159qaz159@gmail.com>
2020-12-20 00:55:30 +0800

    update

--- Map.hpp
+++ Map.hpp
@@ -1,3 +1,57 @@
 #include "Mapclass.hpp"
+#include "texturemanager.hpp"
+
+int Lv1[6][13] = {
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+}
+
+Map::Map() {
+    dirt = TextureManager::LoadTexture("./img/miku.bmp");
+    grass = TextureManager::LoadTexture("./img/miku.bmp");
+    water = TextureManager::LoadTexture("./img/miku.bmp");
+
+    LoadMap(Lv1);
+
+    src.x = src.y = 0;
+}
 
 
+void Map::LoadMap(int arr[6][13]) {
+    for (int row = 0; row < 6; row++) {
+        for (int colume = 0; colume < 13; colume++) {
+            map[row][colume] = arr[row][colume];
+        }
+    }
+}
+
+void Map::DrawMap() {
+    int type = 0;
+    for (int row = 0; row < 6; row++) {
+        for (int colume = 0; colume < 13; colume++) {
+            type = map[row][colume];
+
+            switch (type) {
+            case 0:
+                TextureManager::Draw(water, src, dest);
+            case 1:
+
+            case 2:
+
+            default:
+
+            }
+        }
+    }
+}
