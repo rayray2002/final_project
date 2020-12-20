31995b3f2f5f4f054f34c3ae76b988025aef79ab
shyu <qaz159qaz159@gmail.com>
2020-12-20 09:40:25 +0800

    update

---
+++ ECS.hpp
@@ -0,0 +1,135 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <algorithm>
+#include <bitset>
+#include <array>
+using namespace std;
+
+
+class Component;
+class Entity;
+
+using ComponentID = std::size_t;
+
+inline ComponentID getComponentTypeID() {
+    static ComponentID lastID = 0;
+    return lastID++;
+}
+
+template <typename T> inline ComponentID getComponentTypeID() noexcept {
+    static ComponentID typeID = getComponentTypeID();
+    return typeID();
+}
+
+constexpr std::size_t maxComponents = 32;
+
+using ComponentBitset = std::bitset<maxcomponents>;
+using ComponentArray = std::array<Component*, maxComponents>;
+
+
+class Component {
+public:
+    Entity* entity;
+
+    virtual void init() {};
+    virtual void update() {};
+    virtual void draw() {};
+
+    virtual ~Component() {};
+
+private:
+
+
+};
+
+
+class Entity {
+private:
+    bool active = true;
+    vector<unique_ptr<Component>> components;
+
+    ComponentArray componentArray;
+    ComponentBitset componentBitset;
+public:
+    void Update() {
+        for (auto &c : components) {
+            c->update();
+        }
+        for (auto &c : components) {
+            c->draw();
+        }
+    } 
+
+    void draw() {
+
+    }
+
+    bool isActive() const {
+        return active;
+    }
+
+    void destroy() {
+        active = false;
+    }
+
+    template <typename T> bool hasComponent() const {
+        return ComponentBitset[getComponentID<T>];
+    }
+
+    template <typename T, typename... TArgs>
+    T& addComponent(TArgs&&... mArgs) {
+        T* c(new T(forward<TArgs>(mArgs)...));
+        c->entity = this;
+        unique_ptr<Component> uPtr{ c };
+        components.emplace_back(move(uPtr));
+
+        componentArray[getComponentTypeID<T>()] = c;
+        componentBitset[getComponentTypeID<T>()] = true;
+
+        c->init();
+        return *c;
+    } 
+
+    template<typename T> T& getComponent() const {
+        auto ptr(componentArray[getComponentTypeID<T>()]);
+        return *static_cast<T*>(ptr);
+    }
+
+};
+
+class Manager {
+private:
+    vector<unique_ptr<Entity>> entities;
+
+public:
+    void update() {
+        for (auto &e : entities) {
+            e->update();
+        }
+    }
+
+    void draw() {
+        for (auto &e : entities) {
+            e->draw();
+        }
+    }
+
+    void refresh() {
+        entities.erase( remove_if( begin(entities), end(entities), 
+            [](const unique_ptr<Entity> &mEntity) 
+            {
+            return !mEntity->isActive();
+            } ),
+            end(entities) );
+    }
+
+    Entity& addEntity() {
+        Entity* e = new Entity();
+        unique_ptr<Entity> uPtr{ e };
+        entities.emplace_back(move(uPtr));
+        return *e;
+    }
+};
