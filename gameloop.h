3360bea91a258f719675facacaeb7f9c7649d5ad
shyu <qaz159qaz159@gmail.com>
2020-12-21 16:53:17 +0800

    update

--- gameloop.h
+++ gameloop.h
@@ -12,8 +12,8 @@
 #include <map>
 #include <vector>
 #define NUMMENU 5
-#define WIDTH 1600
-#define HEIGHT 1000
+#define WIDTH 1280
+#define HEIGHT 720
 using namespace std;
 
 class ColliderComponent;
