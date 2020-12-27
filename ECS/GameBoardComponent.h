80a47064e2019e9c44dc0e77ab090c4eea0907a9
rayray2002 <rayray2002.huang@gmail.com>
2020-12-27 23:04:08 +0800

    12/27 ray merge

--- ECS/GameBoardComponent.h
+++ ECS/GameBoardComponent.h
@@ -16,45 +16,40 @@
 
 using namespace std;
 
-class GameBoardComponent : public Component
-{
+class GameBoardComponent : public Component {
 private:
-    char map[14][8];
+//	char map[14][8];
+    GameBoard gameboard;
 
-public: //
-    struct unit
-    {
+public:
+    struct unit {
         SDL_Texture *texture;
         SDL_Rect srcR, destR;
         Vector2D speed;
         Vector2D bspeed;
         Vector2D mapPosition;
-        int color;
+        Color color;
         bool isMoving = true;
         bool isActive = true;
     };
     deque<unit> blocks;
 
-    GameBoardComponent(int xpos, int ypos, int color)
-    {
+    GameBoardComponent(int xpos, int ypos, int color) {
         setInitialMap();
     }
-    ~GameBoardComponent()
-    {
+
+    ~GameBoardComponent() {
     }
-    void setInitialMap()
-    {
-        for (int i = 0; i < 14; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                map[i][j] = '0';
+
+    void setInitialMap() {
+        for (int i = 0; i < 14; i++) {
+            for (int j = 0; j < 8; j++) {
+                gameboard.board[i][j] = Empty;
             }
         }
     }
 
-    void initBlock(int randNumber, int xpos)
-    {
+    void initBlock(int randNumber, int xpos) {
         unit a;
 
         a.destR.x = xpos;
@@ -67,7 +62,23 @@ public: //
         a.srcR.h = 1000;
         a.srcR.w = 1000;
 
-        a.color = randNumber % 4 + 1;
+        switch (randNumber % 4) {
+            case 0:
+                a.color = Red;
+                break;
+            case 1:
+                a.color = Green;
+                break;
+            case 2:
+                a.color = Yellow;
+                break;
+            case 3:
+                a.color = Purple;
+                break;
+            case 4:
+                a.color = Blue;
+                break;
+        }
 
         a.isActive = true;
         a.isMoving = true;
@@ -84,62 +95,60 @@ public: //
         blocks.push_back(a);
     }
 
-    unit *getDataByMapPosition(int ypos, int xpos)
-    {
-        for (auto &b : blocks)
-        {
-            if (b.mapPosition.x == xpos && b.mapPosition.y == ypos)
-            {
+    unit *getDataByMapPosition(int ypos, int xpos) {
+        for (auto &b : blocks) {
+            if (b.mapPosition.x == xpos && b.mapPosition.y == ypos) {
                 return &b;
             }
         }
     }
 
-    void init() override
-    {
+    void init() override {
         initBlock(rand(), 150);
         srand(time(0));
         initBlock(rand(), 200);
     }
 
-    void update() override
-    {
+    void update() override {
         setInitialMap();
         for (auto &a : blocks)
-            map[(a.destR.y - 35) / 50][(a.destR.x - 100) / 50 + 1] = a.color + '0';
-        for (auto &a : blocks)
-        {
+            gameboard.board[(a.destR.y - 35) / 50][(a.destR.x - 100) / 50 + 1] = a.color;
+
+        for (auto &a : blocks) {
             a.mapPosition.x = (a.destR.x - 100) / 50 + 1;
             a.mapPosition.y = (a.destR.y - 35) / 50;
         }
         static int num = 0;
-        for (auto &a : blocks)
-        {
+        for (auto &a : blocks) {
             if (a.speed.y == 0)
                 a.isMoving = false;
-            if (num % 10 == 0 && a.isMoving)
-            {
+            if (num % 10 == 0 && a.isMoving) {
                 if (Game::event.type == SDL_KEYDOWN)
-                    switch (Game::event.key.keysym.sym)
-                    {
-                    case SDLK_LEFT:
-                        if (a.destR.x >= 150 && (map[(a.destR.y - 35) / 50][(a.destR.x - 100) / 50] == '0' || (getDataByMapPosition((a.destR.y - 35) / 50, (a.destR.x - 100) / 50)->isMoving && getDataByMapPosition((a.destR.y - 35) / 50, (a.destR.x - 100) / 50)->mapPosition.x >= 2)))
-                            a.destR.x -= a.bspeed.y;
-                        break;
-                    case SDLK_RIGHT:
-                        if (a.destR.x <= 300 && (map[(a.destR.y - 35) / 50][(a.destR.x - 100) / 50 + 2] == '0' || (getDataByMapPosition((a.destR.y - 35) / 50, (a.destR.x - 100) / 50 + 2)->isMoving && getDataByMapPosition((a.destR.y - 35) / 50, (a.destR.x - 100) / 50 + 2)->mapPosition.x <= 5)))
-                            a.destR.x += a.bspeed.x;
-                        break;
-                    case SDLK_DOWN:
-                        if (map[(a.destR.y - 35) / 50 + 1][(a.destR.x - 100) / 50 + 1] == '0')
-                            a.destR.y += a.speed.y;
-                        break;
-                    case SDLK_SPACE:
-                        break;
-                    default:
-                        break;
+                    switch (Game::event.key.keysym.sym) {
+                        case SDLK_LEFT:
+                            if (a.destR.x >= 150 &&
+                                (gameboard.board[(a.destR.y - 35) / 50][(a.destR.x - 100) / 50] == Empty ||
+                                 (getDataByMapPosition((a.destR.y - 35) / 50, (a.destR.x - 100) / 50)->isMoving &&
+                                  getDataByMapPosition((a.destR.y - 35) / 50, (a.destR.x - 100) / 50)->mapPosition.x >= 2)))
+                                a.destR.x -= a.bspeed.y;
+                            break;
+                        case SDLK_RIGHT:
+                            if (a.destR.x <= 300 &&
+                                (gameboard.board[(a.destR.y - 35) / 50][(a.destR.x - 100) / 50 + 2] == Empty ||
+                                 (getDataByMapPosition((a.destR.y - 35) / 50, (a.destR.x - 100) / 50 + 2)->isMoving &&
+                                  getDataByMapPosition((a.destR.y - 35) / 50, (a.destR.x - 100) / 50 + 2)->mapPosition.x <= 5)))
+                                a.destR.x += a.bspeed.x;
+                            break;
+                        case SDLK_DOWN:
+                            if (gameboard.board[(a.destR.y - 35) / 50 + 1][(a.destR.x - 100) / 50 + 1] == Empty)
+                                a.destR.y += a.speed.y;
+                            break;
+                        case SDLK_SPACE:
+                            break;
+                        default:
+                            break;
                     }
-                else if (num == 60)
+                else if (num % 60 == 0)
                     a.destR.y += a.speed.y;
             }
         }
@@ -148,38 +157,53 @@ public: //
         num++;
     }
 
-    void draw() override
-    {
-        for (auto &a : blocks)
-        {
-            if (a.isActive)
-            {
+    void draw() override {
+        for (auto &a : blocks) {
+            if (a.isActive && a.color != Empty) {
                 SDL_Surface *tmpSurface;
-                if (a.color == 1)
-                    tmpSurface = SDL_LoadBMP("./img/padoru1.bmp");
-                else if (a.color == 2)
-                    tmpSurface = SDL_LoadBMP("./img/padoru2.bmp");
-                else if (a.color == 3)
-                    tmpSurface = SDL_LoadBMP("./img/padoru3.bmp");
+                if (a.color == Red)
+                    tmpSurface = SDL_LoadBMP("./img/sample_red.bmp");
+                else if (a.color == Green)
+                    tmpSurface = SDL_LoadBMP("./img/sample_green.bmp");
+                else if (a.color == Yellow)
+                    tmpSurface = SDL_LoadBMP("./img/sample_yellow.bmp");
+                else if (a.color == Purple)
+                    tmpSurface = SDL_LoadBMP("./img/sample_purple.bmp");
+                else if (a.color == Blue)
+                    tmpSurface = SDL_LoadBMP("./img/sample_blue.bmp");
                 else
-                    tmpSurface = SDL_LoadBMP("./img/padoru4.bmp");
+                    tmpSurface = SDL_LoadBMP("./img/banana.bmp");
                 a.texture = SDL_CreateTextureFromSurface(Game::renderer, tmpSurface);
                 SDL_FreeSurface(tmpSurface);
                 TextureManager::Draw(a.texture, a.srcR, a.destR);
             }
         }
 
-        for (int i = 0; i < 13; i++)
-        {
-            for (int j = 0; j < 8; j++)
-            {
-                cout << map[i][j] << " ";
+        for (int i = 0; i < 13; i++) {
+            for (int j = 0; j < 8; j++) {
+                cout << color_to_char(gameboard.board[i][j]) << " ";
             }
-            cout << endl;
+            cout << '\n';
         }
 
         cout << "\n************************\n\n";
     }
+
+    bool chaining() {
+        bool changed = gameboard.update();
+
+        if (changed) {
+            for (int i = 0; i < blocks.size(); i++) {
+                auto a = blocks[i];
+                if (gameboard.board[(a.destR.y - 35) / 50 + 1][(a.destR.x - 100) / 50 + 1] == Empty) {
+                    blocks.erase(blocks.begin() + i);
+                    i--;
+                }
+            }
+        }
+        return changed;
+    }
+
 };
 
 #endif
