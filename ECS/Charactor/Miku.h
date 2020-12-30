9571656b45695d38643d5807d182a70bb837f151
shyu <qaz159qaz159@gmail.com>
2020-12-30 22:28:53 +0800

    OK

---
+++ ECS/Charactor/Miku.h
@@ -0,0 +1,12 @@
+#ifndef _MIKU_H
+#define _MIKU_H
+
+#include "../CharactorComponent.h"
+
+class Miku : public CharactorComponent
+{
+public:
+    void Skill() override;
+    void Initialize() override;
+};
+#endif
