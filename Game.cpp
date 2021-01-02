80d202d9144e2380e02c134cc2d899fa9c1fd8bc
shyu <qaz159qaz159@gmail.com>
2021-01-02 17:11:51 +0800

    NO

--- Game.cpp
+++ Game.cpp
@@ -29,7 +29,8 @@ auto &gameboard2(manager.addEntity());
 
 vector<Entity *> gameblock;
 
-Game::Game() {
+Game::Game()
+{
 	Character[0] = "Miku";
 	Character[1] = "Kirito";
 	Character[2] = "Asuna";
@@ -37,33 +38,41 @@ Game::Game() {
 	Character[4] = "Heish";
 }
 
-Game::~Game() {
+Game::~Game()
+{
 }
 
-void Game::init(const char *title, int xMenuPos, int yMenuPos, int width, int height, bool fullscreen) {
+void Game::init(const char *title, int xMenuPos, int yMenuPos, int width, int height, bool fullscreen)
+{
 
 	int flags = 0;
 
-	if (fullscreen) {
+	if (fullscreen)
+	{
 		flags = SDL_WINDOW_FULLSCREEN;
 	}
 
-	if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
+	if (SDL_Init(SDL_INIT_EVERYTHING) == 0)
+	{
 		cout << "Subsystem Initialised!..." << endl;
 
 		window = SDL_CreateWindow(title, xMenuPos, yMenuPos, width, height, flags);
-		if (window) {
+		if (window)
+		{
 			cout << "Window Created!" << endl;
 		}
 
 		Game::renderer = SDL_CreateRenderer(window, -1, 0);
-		if (Game::renderer) {
+		if (Game::renderer)
+		{
 			SDL_SetRenderDrawColor(Game::renderer, 255, 255, 255, 255);
 			cout << "Renderer Created!" << endl;
 		}
 
 		isRunning = true;
-	} else {
+	}
+	else
+	{
 		isRunning = false;
 	}
 
@@ -111,34 +120,62 @@ auto &buttoms(manager.getGroup(Game::groupButtoms));
 auto &gameboards(manager.getGroup(Game::groupGameBoards));
 auto &gameboards2(manager.getGroup(Game::groupDoubleGameBoards));
 
-void Game::handleEveants() {
+void Game::handleEveants()
+{
 	SDL_PollEvent(&event);
 
-	switch (event.type) {
-		case SDL_QUIT:
-			isRunning = false;
-			break;
-		case SDLK_SPACE:
-			isRunning = false;
-			break;
-		default:
-			break;
+	switch (event.type)
+	{
+	case SDL_QUIT:
+		isRunning = false;
+		break;
+	case SDLK_SPACE:
+		isRunning = false;
+		break;
+	default:
+		break;
 	}
 }
 
-void Game::update() {
+void Game::update()
+{
 	manager.refresh();
 	manager.update();
 }
 
-void Game::render() {
+void Game::render()
+{
 
-	for (auto &b : backs) {
+	for (auto &b : backs)
+	{
 		b->getComponent<BackGroundComponent>().draw();
 	}
 
-	for (auto &b : buttoms) {
+	static int Tick1 = 0;
+	static int Tick2 = 0;
+	if (Tick1 == 0)
+	{
+		Tick1 = SDL_GetTicks();
+	}
+
+	if (Tick2 - Tick1 <= 10 * 1000)
+	{
+	}
+	for (auto &b : buttoms)
+	{
 		b->getComponent<ButtomComponent>().draw();
+	}
+
+	for (auto &b : gameboards2)
+	{
+		b->getComponent<DoubleGameBoardComponent>().draw();
+	}
+
+	Tick2 = SDL_GetTicks();
+	if (Tick2 - Tick1 >= 10 * 1000)
+	{
+		Tick1 = SDL_GetTicks();
+		cout << "TIME UP" << endl;
 	}
 
 	// for (auto &b : textbuttoms)
@@ -149,15 +186,13 @@ void Game::render() {
 	// {
 	//     b->getComponent<GameBoardComponent>().draw();
 	// }
-	for (auto &b : gameboards2) {
-		b->getComponent<DoubleGameBoardComponent>().draw();
-	}
 
 	SDL_RenderPresent(Game::renderer);
 	SDL_RenderClear(Game::renderer);
 }
 
-void Game::clean() {
+void Game::clean()
+{
 	SDL_DestroyWindow(window);
 	SDL_DestroyRenderer(renderer);
 	Mix_CloseAudio();
@@ -167,7 +202,8 @@ void Game::clean() {
 	cout << "Game Cleaned!" << endl;
 }
 
-void Game::showmenu() {
+void Game::showmenu()
+{
 
 	const int FPS = 60;
 	const int frameDelay = 1000 / FPS;
@@ -176,19 +212,22 @@ void Game::showmenu() {
 	int frameTime; //int
 
 	MenuisRunning = true;
-	if (TTF_Init() == -1) {
+	if (TTF_Init() == -1)
+	{
 	}
 
 	//Set Character Position Here (Start)
 	int CharacterHeight = 160;
-	for (int i = 0; i < NUMMENU; i++) {
+	for (int i = 0; i < NUMMENU; i++)
+	{
 		MenuCharacterPos[i].y = HEIGHT - 80 - CharacterHeight;
 		MenuCharacterPos[i].h = CharacterHeight;
 		MenuCharacterPos[i].w = 100;
 	}
 
 	MenuCharacterPos[0].x = 30;
-	for (int i = 1; i < NUMMENU; i++) {
+	for (int i = 1; i < NUMMENU; i++)
+	{
 		MenuCharacterPos[i].x = MenuCharacterPos[i - 1].x + 130;
 	}
 	//Set Character Position Here (End)
@@ -201,7 +240,8 @@ void Game::showmenu() {
 	bool selected[NUMMENU] = {0};
 
 	//Set Menu Position Here (Start)
-	for (int i = 0; i < NUMMENU; i++) {
+	for (int i = 0; i < NUMMENU; i++)
+	{
 		MenuPos[i].h = 50;
 		MenuPos[i].w = 100;
 	}
@@ -210,23 +250,27 @@ void Game::showmenu() {
 	int DOWN = HEIGHT - 30;
 	int LEFT[NUMMENU] = {30, 160, 290, 420, 550};
 	int RIGHT[NUMMENU] = {130, 260, 390, 520, 650};
-	for (int i = 0; i < NUMMENU; i++) {
+	for (int i = 0; i < NUMMENU; i++)
+	{
 		MenuLabel[i] << Character[i];
 	}
 
-	for (int i = 0; i < NUMMENU; i++) {
+	for (int i = 0; i < NUMMENU; i++)
+	{
 		MenuChoice[i] = TTF_RenderText_Solid(MenuFont, MenuLabel[i].str().c_str(), MenuColor[0]);
 		MenuPos[i].y = HEIGHT - 60;
 	}
 
 	MenuPos[0].x = 30;
-	for (int i = 1; i < NUMMENU; i++) {
+	for (int i = 1; i < NUMMENU; i++)
+	{
 		MenuPos[i].x = MenuPos[i - 1].x + 130;
 	}
 	//Set Menu Position Here (End)
 
 	//Title (Character) Basic Setting (Start)
-	for (int i = 0; i < NUMMENU; i++) {
+	for (int i = 0; i < NUMMENU; i++)
+	{
 		CharacterTextRect[i].h = 70;
 		CharacterTextRect[i].w = 600;
 		CharacterTextRect[i].x = 50;
@@ -235,7 +279,8 @@ void Game::showmenu() {
 
 	CharacterTextFont = TTF_OpenFont("./fonts/SAOUITT-Regular.ttf", 1000);
 	TTF_SetFontStyle(CharacterTextFont, TTF_STYLE_ITALIC);
-	for (int i = 0; i < NUMMENU; i++) {
+	for (int i = 0; i < NUMMENU; i++)
+	{
 		CharacterTextText[i].str("");
 	}
 	CharacterTextText[0] << "Miku: Miku Yu Yu!!!";
@@ -250,7 +295,8 @@ void Game::showmenu() {
 
 	//Main Loop (Start)
 	SDL_Event event;
-	while (1) {
+	while (1)
+	{
 		SDL_Delay(1000);
 		//Title Basic Setting (Start)
 		SDL_Texture *LinkStartTexture = NULL;
@@ -260,12 +306,14 @@ void Game::showmenu() {
 		dst.w = 600;
 		dst.x = 30;
 		dst.y = 30;
-		if (TTF_Init() == -1) {
+		if (TTF_Init() == -1)
+		{
 			cout << "TTF_Init: " << TTF_GetError() << endl;
 		}
 		TTF_Font *font;
 		font = TTF_OpenFont("./fonts/SAOUITT-Regular.ttf", 1000);
-		if (!font) {
+		if (!font)
+		{
 			cout << "TTF_OpenFont: " << TTF_GetError() << endl;
 		}
 		TTF_SetFontStyle(font, TTF_STYLE_ITALIC);
@@ -276,7 +324,8 @@ void Game::showmenu() {
 		LinkStartText << "Choose Character";
 		LinkStartSurface = TTF_RenderText_Solid(font, LinkStartText.str().c_str(), color);
 		LinkStartTexture = SDL_CreateTextureFromSurface(renderer, LinkStartSurface);
-		if (!LinkStartTexture) {
+		if (!LinkStartTexture)
+		{
 			SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
 		}
 		SDL_FreeSurface(LinkStartSurface);
@@ -285,12 +334,14 @@ void Game::showmenu() {
 		//Title Basic Setting (End)
 
 		//Title (Character) Basic Setting (Start)
-		for (int i = 0; i < NUMMENU; i++) {
+		for (int i = 0; i < NUMMENU; i++)
+		{
 			SDL_FreeSurface(CharacterTextSurface[i]);
 		}
-		for (int i = 0; i < NUMMENU; i++) {
+		for (int i = 0; i < NUMMENU; i++)
+		{
 			CharacterTextSurface[i] = TTF_RenderText_Solid(CharacterTextFont, CharacterTextText[i].str().c_str(),
-			                                               CharacterTextColor);
+														   CharacterTextColor);
 		}
 
 		//Title (Character) Basic Setting (End)
@@ -298,7 +349,8 @@ void Game::showmenu() {
 		//Character Setting (Start)
 		frameStart = SDL_GetTicks();
 
-		for (int i = 0; i < NUMMENU; i++) {
+		for (int i = 0; i < NUMMENU; i++)
+		{
 			SDL_FreeSurface(MenuCharacterSurface[i]);
 		}
 		MenuCharacterSurface[0] = SDL_LoadBMP("./img/miku.bmp");
@@ -307,70 +359,89 @@ void Game::showmenu() {
 		MenuCharacterSurface[3] = SDL_LoadBMP("./img/miku.bmp");
 		MenuCharacterSurface[4] = SDL_LoadBMP("./img/miku.bmp");
 		//Character Setting (End)
-		for (int i = 0; i < NUMMENU; i++) {
+		for (int i = 0; i < NUMMENU; i++)
+		{
 			MenuChoice[i] = TTF_RenderText_Solid(MenuFont, MenuLabel[i].str().c_str(), MenuColor[1]);
 		}
 
 		//Event Loop (Start)
-		while (SDL_PollEvent(&event) != 0) {
-			switch (event.type) {
-				case SDL_QUIT:
-					for (int i = 0; i < NUMMENU; i++) {
-						SDL_FreeSurface(MenuChoice[i]);
-						MenuisRunning = false;
-					}
-					break;
-				case SDL_MOUSEMOTION:
-					X_MENU_MOUSE = event.motion.x;
-					Y_MENU_MOUSE = event.motion.y;
+		while (SDL_PollEvent(&event) != 0)
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				for (int i = 0; i < NUMMENU; i++)
+				{
+					SDL_FreeSurface(MenuChoice[i]);
+					MenuisRunning = false;
+				}
+				break;
+			case SDL_MOUSEMOTION:
+				X_MENU_MOUSE = event.motion.x;
+				Y_MENU_MOUSE = event.motion.y;
 
-					for (int i = 0; i < NUMMENU; i++) {
-						if (X_MENU_MOUSE >= LEFT[i] && X_MENU_MOUSE <= RIGHT[i] && Y_MENU_MOUSE >= UP &&
-						    Y_MENU_MOUSE <= DOWN) {
-							if (!selected[i]) {
-								selected[i] = 1;
+				for (int i = 0; i < NUMMENU; i++)
+				{
+					if (X_MENU_MOUSE >= LEFT[i] && X_MENU_MOUSE <= RIGHT[i] && Y_MENU_MOUSE >= UP &&
+						Y_MENU_MOUSE <= DOWN)
+					{
+						if (!selected[i])
+						{
+							selected[i] = 1;
+							SDL_FreeSurface(MenuChoice[i]);
+							MenuChoice[i] = TTF_RenderText_Solid(MenuFont, MenuLabel[i].str().c_str(),
+																 MenuColor[1]);
+						}
+						else
+						{
+							if (selected[i])
+							{
+								selected[i] = 0;
 								SDL_FreeSurface(MenuChoice[i]);
 								MenuChoice[i] = TTF_RenderText_Solid(MenuFont, MenuLabel[i].str().c_str(),
-								                                     MenuColor[1]);
-							} else {
-								if (selected[i]) {
-									selected[i] = 0;
-									SDL_FreeSurface(MenuChoice[i]);
-									MenuChoice[i] = TTF_RenderText_Solid(MenuFont, MenuLabel[i].str().c_str(),
-									                                     MenuColor[0]);
-								}
-							}
-						} else {
-							selected[i] = 0;
-							SDL_FreeSurface(MenuChoice[i]);
-							MenuChoice[i] = TTF_RenderText_Solid(MenuFont, MenuLabel[i].str().c_str(), MenuColor[0]);
-						}
-					}
-					break;
-				case SDL_MOUSEBUTTONDOWN:
-					X_MENU_MOUSE = event.motion.x;
-					Y_MENU_MOUSE = event.motion.y;
-					for (int i = 0; i < NUMMENU; i++) {
-						if (X_MENU_MOUSE >= MenuPos[i].x && X_MENU_MOUSE <= MenuPos[i].x + MenuPos[i].w &&
-						    Y_MENU_MOUSE >= MenuPos[i].y && Y_MENU_MOUSE <= MenuPos[i].y + MenuPos[i].h) {
-							for (int j = 0; j < NUMMENU; j++) {
-								SDL_FreeSurface(MenuChoice[j]);
+																	 MenuColor[0]);
 							}
 						}
 					}
-					break;
-				case SDL_KEYDOWN:
-					if (event.key.keysym.sym == SDLK_ESCAPE) {
-						for (int i = 0; i < NUMMENU; i++) {
-							SDL_FreeSurface(MenuChoice[i]);
-						}
-						MenuisRunning = false;
+					else
+					{
+						selected[i] = 0;
+						SDL_FreeSurface(MenuChoice[i]);
+						MenuChoice[i] = TTF_RenderText_Solid(MenuFont, MenuLabel[i].str().c_str(), MenuColor[0]);
 					}
-					break;
+				}
+				break;
+			case SDL_MOUSEBUTTONDOWN:
+				X_MENU_MOUSE = event.motion.x;
+				Y_MENU_MOUSE = event.motion.y;
+				for (int i = 0; i < NUMMENU; i++)
+				{
+					if (X_MENU_MOUSE >= MenuPos[i].x && X_MENU_MOUSE <= MenuPos[i].x + MenuPos[i].w &&
+						Y_MENU_MOUSE >= MenuPos[i].y && Y_MENU_MOUSE <= MenuPos[i].y + MenuPos[i].h)
+					{
+						for (int j = 0; j < NUMMENU; j++)
+						{
+							SDL_FreeSurface(MenuChoice[j]);
+						}
+					}
+				}
+				break;
+			case SDL_KEYDOWN:
+				if (event.key.keysym.sym == SDLK_ESCAPE)
+				{
+					for (int i = 0; i < NUMMENU; i++)
+					{
+						SDL_FreeSurface(MenuChoice[i]);
+					}
+					MenuisRunning = false;
+				}
+				break;
 			}
 		}
-		if (!MenuisRunning) {
-			for (int i = 0; i < NUMMENU; i++) {
+		if (!MenuisRunning)
+		{
+			for (int i = 0; i < NUMMENU; i++)
+			{
 				SDL_DestroyTexture(MenuTex[i]);
 				SDL_DestroyTexture(MenuCharacterTex[i]);
 				SDL_DestroyTexture(CharacterTextTexture[i]);
@@ -382,7 +453,8 @@ void Game::showmenu() {
 		//Render (Start)
 		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0x00);
 		SDL_RenderClear(renderer);
-		for (int i = 0; i < NUMMENU; i++) {
+		for (int i = 0; i < NUMMENU; i++)
+		{
 
 			MenuCharacterTex[i] = SDL_CreateTextureFromSurface(renderer, MenuCharacterSurface[i]);
 			CharacterTextTexture[i] = SDL_CreateTextureFromSurface(renderer, CharacterTextSurface[i]);
@@ -403,14 +475,16 @@ void Game::showmenu() {
 		//Render (End)
 
 		frameTime = SDL_GetTicks() - frameStart;
-		if (frameDelay > frameTime) { //make the screen more smooth
+		if (frameDelay > frameTime)
+		{ //make the screen more smooth
 			SDL_Delay(frameDelay - frameTime);
 		}
 	}
 	//Main Loop (End)
 }
 
-void Game::LinkStart(string text, int second, int h, int w) {
+void Game::LinkStart(string text, int second, int h, int w)
+{
 	SDL_Texture *LinkStartTexture = NULL;
 	SDL_Surface *LinkStartSurface = NULL;
 	SDL_Rect dst;
@@ -418,12 +492,14 @@ void Game::LinkStart(string text, int second, int h, int w) {
 	dst.w = w;
 	dst.x = WIDTH / 2 - dst.w / 2;
 	dst.y = HEIGHT / 2 - dst.h / 2;
-	if (TTF_Init() == -1) {
+	if (TTF_Init() == -1)
+	{
 		cout << "TTF_Init: " << TTF_GetError() << endl;
 	}
 	TTF_Font *font;
 	font = TTF_OpenFont("./fonts/SAOUITT-Regular.ttf", 1000);
-	if (!font) {
+	if (!font)
+	{
 		cout << "TTF_OpenFont: " << TTF_GetError() << endl;
 	}
 	TTF_SetFontStyle(font, 0 /*TTF_STYLE_BOLD|TTF_STYLE_ITALIC*/);
@@ -434,7 +510,8 @@ void Game::LinkStart(string text, int second, int h, int w) {
 	LinkStartText << text;
 	LinkStartSurface = TTF_RenderText_Solid(font, LinkStartText.str().c_str(), color);
 	LinkStartTexture = SDL_CreateTextureFromSurface(renderer, LinkStartSurface);
-	if (!LinkStartTexture) {
+	if (!LinkStartTexture)
+	{
 		SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't create texture from surface: %s", SDL_GetError());
 	}
 	SDL_FreeSurface(LinkStartSurface);
@@ -449,15 +526,18 @@ void Game::LinkStart(string text, int second, int h, int w) {
 	cout << text << endl;
 }
 
-void Game::MusicPlay(const char *Music, int volume) {
+void Game::MusicPlay(const char *Music, int volume)
+{
 	// load support for the MP3 sample/music formats
 	int mflags = MIX_INIT_MP3;
-	if ((Mix_Init(mflags) & mflags) != mflags) {
+	if ((Mix_Init(mflags) & mflags) != mflags)
+	{
 		// cout << "Mix_Init: Failed to init required ogg and mod support!\n";
 		// cout << "Mix_Init: %s\n" << Mix_GetError() << endl;
 	}
 
-	if (Mix_OpenAudio(128000, MIX_DEFAULT_FORMAT, 2, 2048) == -1) {
+	if (Mix_OpenAudio(128000, MIX_DEFAULT_FORMAT, 2, 2048) == -1)
+	{
 		// cout << "Mix_OpenAudio: %s\n" << Mix_GetError() << endl;
 	}
 
@@ -465,9 +545,10 @@ void Game::MusicPlay(const char *Music, int volume) {
 	Mix_Music *music;
 	music = Mix_LoadMUS(Music);
 	//Mix_LoadMUS
-	if (!music) {
+	if (!music)
+	{
 		cout << "Mix_LoadMUS(\"miku.mp3\"): %s\n"
-		     << Mix_GetError();
+			 << Mix_GetError();
 	}
 	Mix_VolumeMusic(volume);
 	Mix_PlayMusic(music, -1);
