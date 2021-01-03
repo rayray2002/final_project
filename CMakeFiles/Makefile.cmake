1d7b2afe990c219b006acff91a94b746b82f476e
shyu <qaz159qaz159@gmail.com>
2021-01-03 22:54:36 +0800

    OK

--- CMakeFiles/Makefile.cmake
+++ CMakeFiles/Makefile.cmake
@@ -55,5 +55,5 @@ set(CMAKE_MAKEFILE_PRODUCTS
 
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
-  "CMakeFiles/main.dir/DependInfo.cmake"
+  "CMakeFiles/menu.dir/DependInfo.cmake"
   )
